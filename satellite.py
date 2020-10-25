5bb620be296337ebfc2cc23f8767326f0bea3727
Matias Rietig <rietig.m@googlemail.com>
2020-10-25 16:22:51 +0100

    added tensorflow script for vessel detection training

--- satellite.py
+++ satellite.py
@@ -1,24 +1,18 @@
 import rasterio
 from rasterio.plot import show
-from rasterio.plot import show_hist
 from rasterio import plot
 from rasterio.enums import Resampling
 from rasterio.warp import reproject, Resampling
-from rasterio import mask
 from rasterio.windows import Window
-import rasterio.features
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from shapely.geometry import shape, mapping, Polygon
-import fiona
-from fiona.crs import from_epsg
 from scipy import ndimage, misc, signal
 
 downscale_factor = 1/2
 
 ## TCI downscaled to 20m ppx
-with rasterio.open("tci.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as dataset:
+with rasterio.open("../tci.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as dataset:
     windows_d1 = []
     windows_d1.append(dataset.read(window=Window(2500, 3000, 2500, 1500),
                             out_shape=(
@@ -37,7 +31,7 @@ with rasterio.open("tci.jp2", tiled=True, blockxsize=256, blockysize=256, num_th
 
 
 ## MIR image with 20m ppx
-with rasterio.open("channel11.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as dataset2:
+with rasterio.open("../swir.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as dataset2:
     windows_d2 = []
     windows_d2.append(dataset2.read(window=Window(1250, 1500, 1250, 750)))
     windows_d2.append(dataset2.read(window=Window(3250, 3500, 1250, 750)))
@@ -56,7 +50,7 @@ for i in range(len(windows_d1)):
     zoomed.append(ndimage.zoom(result[i], (1, 2, 2), order=0))
 
 ## Open full TCI image
-with rasterio.open("tci.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as src:
+with rasterio.open("../tci.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as src:
     windows_src = []
     windows_src.append(src.read(window=Window(2500, 3000, 2500, 1500)))
     windows_src.append(src.read(window=Window(6500, 7000, 2500, 1500)))
@@ -66,5 +60,40 @@ filtered_src = []
 ## Apply MNDWI mask
 for i in range(len(windows_src)):
     filtered_src.append(np.where(zoomed[i] == 1, windows_src[i], 0))
-    plot.show(windows_src[i])
-    plot.show(filtered_src[i])
+
+
+## Plot results (TCI vs. masked image)
+##fig, ((ax1, bx1), (ax2, bx2)) = plt.subplots(nrows=2, ncols=2)
+##show(windows_src[0], ax=ax1, title='route 1 true color')
+##show(windows_src[1], ax=ax2, title='route 2 true color')
+##show(filtered_src[0], ax=bx1, title='route 1 water filter')
+##show(filtered_src[1], ax=bx2, title='route 2 water filter')
+##plt.show()
+
+## Vessel detection
+## Vessel Index VI = NIR - Red Channel
+## Open NIR image
+with rasterio.open("../nir.jp2", tiled=True, blockxsize=256, blockysize=256, num_threads='all_cpus') as nir:
+    windows_nir = []
+    windows_nir.append(nir.read(window=Window(2500, 3000, 2500, 1500)))
+    windows_nir.append(nir.read(window=Window(6500, 7000, 2500, 1500)))
+
+vi = []
+vessels_src = []
+for i in range(len(windows_d1)):
+    vi.append(windows_nir[i] - filtered_src[i][2])
+    vessels_src.append(np.where(vi[i] > 0, filtered_src[i], 0))
+
+print(windows_nir[0], windows_src[0][2])
+plot.show(windows_nir[0] - windows_src[0][2])
+print(windows_nir[0] - windows_src[0][2])
+
+## Plot results (TCI vs. masked image)
+fig, ((ax1, bx1, cx1), (ax2, bx2, cx2)) = plt.subplots(nrows=2, ncols=3)
+show(windows_src[0], ax=ax1, title='route 1 true color')
+show(windows_src[1], ax=ax2, title='route 2 true color')
+show(filtered_src[0], ax=bx1, title='route 1 water filter')
+show(filtered_src[1], ax=bx2, title='route 2 water filter')
+show(vessels_src[0], ax=cx1, title='route 1 vessel index')
+show(vessels_src[1], ax=cx2, title='route 2 vessel index')
+plt.show()
---
+++ vessel_detector.py
@@ -0,0 +1,111 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+import pathlib
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+data_dir = pathlib.Path("../learning_imgs/")
+ships = list(data_dir.glob('ship/*'))
+
+batch_size = 32
+img_height = 512
+img_width = 512
+
+## Training Dataset
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+## Validation Dataset
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+## Tune for performance
+##AUTOTUNE = tf.data.experimental.AUTOTUNE
+##train_ds = train_ds.cache().shuffle(100).prefetch(buffer_size=AUTOTUNE)
+##val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+## Configure Augmentation
+data_augmentation = keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
+
+
+## Model Definition
+num_classes = 7
+model = Sequential([
+  data_augmentation,
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+## Model Compilation
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+## Model Summary
+model.summary()
+
+## Training
+epochs=15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+## Training Results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
